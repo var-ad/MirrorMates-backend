3852b5e36292f01a4989967e024955753395c90a
var-ad <varad.chaskar.27@gmail.com>
2026-04-05 02:21:59 +0530

    remove dist from tracking

--- dist/modules/payments/subscription.validators.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createSubscriptionSchema = void 0;
-const validate_1 = require("../../middleware/validate");
-exports.createSubscriptionSchema = validate_1.z.object({
-    planId: validate_1.z.string().uuid()
-});
